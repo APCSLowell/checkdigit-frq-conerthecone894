567aaa815e1f9b7ec4b92dfccf63b9b67de1b0f8
conerthecone894 <143015307+conerthecone894@users.noreply.github.com>
2024-04-30 07:19:10 +0000

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -11,7 +11,7 @@ public class CheckDigit
 //==========================================================
      int total = 0;
 
-     for(int i = 1; i < num.getNumberofDigits(num); i++){
+     for(int i = 1; i < getNumberofDigits(num); i++){
        total += getDigit(num, i) * (8-i);
      }
 
